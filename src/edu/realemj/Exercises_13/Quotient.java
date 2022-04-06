9cd61b7968149083a8a5d28ad2ceb556df8e1be9
Dr. Michael J. Reale <realemj@sunypoly.edu>
2022-04-06 13:03:04 -0400

    Started on exceptions

---
+++ src/edu/realemj/Exercises_13/Quotient.java
@@ -0,0 +1,34 @@
+package edu.realemj.Exercises_13;
+
+import java.util.*;
+
+public class Quotient {
+    public static int quotient(int a, int b) {
+        if(b == 0) {
+            throw new ArithmeticException("YOU FOOL!");
+        }
+        int result = a/b;
+        return result;
+    }
+
+    public static void main(String [] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        try {
+            int x = input.nextInt();
+            int y = input.nextInt();
+            int r = quotient(x,y);
+            System.out.println("Result: " + r);
+        }
+        catch(ArithmeticException ex) {
+            System.err.println(ex.getMessage());
+            System.err.println("YOU HAVE DESTROYED THE UNIVERSE!!!");
+        }
+        catch(InputMismatchException ex) {
+            System.err.println("BAD ENTRY!!!!");
+        }
+
+        System.out.println("Life goes on...");
+    }
+    
+}
